8a1098534aec613318188d92d0692db6e250d508
Sean Daweng <sdaweng@myseneca.ca>
2025-07-07 21:10:27 -0400

    adding lab7a

--- lab7a.py
+++ lab7a.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Student ID: [seneca_id]
+# Student ID: sdaweng
 class Time:
     """Simple object type for time of the day.
     data attributes: hour, minute, second
@@ -20,6 +20,26 @@ def sum_times(t1, t2):
     sum.hour = t1.hour + t2.hour
     sum.minute = t1.minute + t2.minute
     sum.second = t1.second + t2.second
+
+
+     #[ insert python code here to check for minute and second 
+    #[ attribute here, and carry over when necessary
+    #[
+    
+    #if second reaches 60 carry over 1 to minute
+    #t1 second = 38 
+    #t2 second = 40
+    #result = 1:18
+
+    if sum.second >= 60:
+        sum.second = sum.second % 60
+        sum.minute += 1
+    
+    
+    if sum.minute >= 60:
+        sum.minute = sum.minute % 60
+        sum.hour += 1
+    
     return sum
 
 def valid_time(t):
@@ -30,3 +50,12 @@ def valid_time(t):
     if t.minute >= 60 or t.second >= 60 or t.hour >= 24:
         return False
     return True
+
+if __name__ == "__main__":
+    t1 = Time(0,1,24)
+    t2 = Time(0,3,59)
+
+    ft = format_time
+
+
+    print(ft(t1) + ' + ' + ft(t2) + ' = ' + ft(sum_times(t1, t2)))
